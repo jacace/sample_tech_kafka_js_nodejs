cac8da7fd583bc91adab794fc1bba9bf63175529
jacace <javierandrescaceres@gmail.com>
2016-03-11 11:48:24 +0000

    fixed project.json file

--- test/test.js
+++ test/test.js
@@ -2,13 +2,13 @@
 
 'use strict';
 
-var request=require("supertest"),
-	app=require("../app").getApp;
+var request = require("supertest"),
+    app = require("../app").getApp;
 
-describe('GET /',function(){
-	it('expects HTTP response 2000', function(done){
-		request(app)
-			.get('/')
-			.expect(200, done);
-	});
+describe('GET /', function(){
+  it('expects HTTP response 200', function(done){
+    request(app)
+     .get('/')
+	 .expect(200, done); 
+  });
 });
