6c1f38c8293a9780e3dde46a5e03bdd76f7dee14
jacace <javierandrescaceres@gmail.com>
2020-08-10 23:04:41 +0100

    Added Sample APIs

---
+++ CommodityIaCPipelineAsCode.js
@@ -0,0 +1,11 @@
+class CommodityAKSPipelineAsCode {
+
+	constructor(initData) {
+		this.initData = initData;
+	}
+
+	run(eventData) {
+		return "EKS public End Point";
+	}
+
+}
---
+++ CommodityNodejsPipelineAsCode.js
@@ -0,0 +1,11 @@
+class CommodityNodejsPipelineAsCode {
+
+	constructor(initData) {
+		this.initData = initData;
+	}
+
+	run(eventData) {
+		return "End Point where NodeJs is running";
+	}
+
+}
