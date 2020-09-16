19f32b0a57207fbc61b801fb24e07d6d4f1cc380
jacace <javierandrescaceres@gmail.com>
2020-09-16 14:24:31 +0100

    Added ENV Vars

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
-//Sample NodeJS app (withut typescript) to illustrate design pattern
+//Sample NodeJS app to illustrate design pattern
 
 'use strict';
 
+//kafka init starts
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+	clientId: 'my-app',
+	brokers: [ process.env.main_broker ],	
+	ssl: true,
+	sasl: {
+		mechanism: process.env.sasl,
+		username:  process.env.kafkaapikey,
+		password:  process.env.kafkaapisecret
+	},
+})
+
+const consumer = kafka.consumer({ groupId: 'confluent_cli_consumer_c5c808c6-c86b-48d8-9390-317df319cc55' })
+consumer.connect()
+consumer.subscribe({
+	topic: 'request_vpc_deployment', fromBeginning: true
+})
+
+//kafka init ends
+
+
+//kafka business logic starts
+
+const producer = kafka.producer()
+producer.send({
+	topic: 'req_to_create_vpc',
+	messages: [
+		{ key: 'vpc-name', value: 'hello world' },
+		{ key: 'vpc-cidr', value: '10.0.0.0/16' },
+		{ key: 'vpc-name', value: 'hello world' },
+		{ key: 'environment', value: 'DEV' },
+		{ key: 'owner', value: 'javier' },
+	],
+})
+
+consumer.run({
+	eachMessage: async ({ topic, partition, message }) => {
+		console.log({
+			value: message.value.toString(),
+		})
+	},
+})
+
+//kafka business logic ends
+
+
 var express = require('express');
-var Kafka = require('no-kafka');
 var app = express();
 
 app.set('views', 'views');
@@ -15,7 +62,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-	let eventType = req.body.eventtype;	
+	let eventType = req.body.eventtype;
 	let eventData = req.body.eventdata;
 	//Note: In real life the EndPoint is retrieved from a Service Registry
 	//      not from with a switch/new like below :) 
@@ -43,7 +90,6 @@ app.post('/', function (req, res) {
 
 	console.log(request.body);
 });
-
 
 app.listen(8080);
 module.exports.getApp = app;
